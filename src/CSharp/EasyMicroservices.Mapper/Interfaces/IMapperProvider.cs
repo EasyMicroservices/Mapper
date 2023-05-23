0e85ac5647fa5416da5871b414d1cb9fd061500c
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-23 15:30:48 +0330

    init interface

---
+++ src/CSharp/EasyMicroservices.Mapper/Interfaces/IMapperProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyMicroservices.Mapper.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMapperProvider
+    {
+        /// <summary>
+        /// map an object to another object
+        /// </summary>
+        /// <typeparam name="TFrom">map from object type</typeparam>
+        /// <typeparam name="TTo">map to object type</typeparam>
+        /// <param name="fromObject">mapped object</param>
+        /// <returns></returns>
+        TTo Map<TFrom, TTo>(TFrom fromObject);
+    }
+}
