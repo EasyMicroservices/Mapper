7b29f40904b0ef088a30cea513e09d9529381db8
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-01 13:17:51 +0330

    add tests

---
+++ src/CSharp/EasyMicroservices.MapGeneration.Tests/Logics/EnvironmentLoaderTest.cs
@@ -0,0 +1,29 @@
+﻿using EasyMicroservices.FileManager.Providers.DirectoryProviders;
+using EasyMicroservices.FileManager.Providers.FileProviders;
+using EasyMicroservices.FileManager.Providers.PathProviders;
+using EasyMicroservices.MapGeneration.Logics;
+using EasyMicroservices.Serialization.Newtonsoft.Json.Providers;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyMicroservices.MapGeneration.Tests.Logics
+{
+    public class EnvironmentLoaderTest
+    {
+        public EnvironmentLoaderTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task Load()
+        {
+            var pathProvider = new SystemPathProvider();
+            var appPath = AppDomain.CurrentDomain.BaseDirectory;
+            EnvironmentLoader loader = new EnvironmentLoader(new NewtonsoftJsonProvider(), new DiskFileProvider(new DiskDirectoryProvider(appPath, pathProvider)));
+            await loader.Load(pathProvider.Combine(appPath, "TestMap.json"));
+            Assert.True(loader.AppData.Environments.Count > 0);
+        }
+    }
+}
