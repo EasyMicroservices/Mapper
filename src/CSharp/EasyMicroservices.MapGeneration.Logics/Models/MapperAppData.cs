7d83edefb8ff10e5e46d70d2ba5ad06f15c31c83
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-01 13:17:45 +0330

    add logics

---
+++ src/CSharp/EasyMicroservices.MapGeneration.Logics/Models/MapperAppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.MapGeneration.Models
+{
+    public class MapperAppData
+    {
+        public List<EnvironmentInfo> Environments { get; set; } = new List<EnvironmentInfo>();
+    }
+}
