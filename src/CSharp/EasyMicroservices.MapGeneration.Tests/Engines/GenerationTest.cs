0ccc93f3f9d747ea49bb229a3f0be2effb11f1ec
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-02 19:42:06 +0330

    add generation test

---
+++ src/CSharp/EasyMicroservices.MapGeneration.Tests/Engines/GenerationTest.cs
@@ -0,0 +1,29 @@
+﻿using EasyMicroservices.FileManager.Providers.DirectoryProviders;
+using EasyMicroservices.FileManager.Providers.FileProviders;
+using EasyMicroservices.FileManager.Providers.PathProviders;
+using EasyMicroservices.MapGeneration.Engines;
+using EasyMicroservices.MapGeneration.Logics;
+using EasyMicroservices.Serialization.Newtonsoft.Json.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyMicroservices.MapGeneration.Tests.Engines
+{
+    public class GenerationTest
+    {
+        [Fact]
+        public async Task TestGeneation()
+        {
+            var pathProvider = new SystemPathProvider();
+            var appPath = AppDomain.CurrentDomain.BaseDirectory;
+            EnvironmentLoader loader = new EnvironmentLoader(new NewtonsoftJsonProvider(), new DiskFileProvider(new DiskDirectoryProvider(appPath, pathProvider)));
+            await loader.Load(pathProvider.Combine(appPath, "TestMap.json"));
+            Generation generation = new Generation(loader.AppData.Environments.First());
+            await generation.Build();
+        }
+    }
+}
