43aa05064336d91f9092c7c952ee6162c529d92c
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-06 15:39:49 +0330

    add support for compile time mapper

---
+++ src/CSharp/EasyMicroservices.Mapper.CompileTimeMapper/Interfaces/IMapper.cs
@@ -0,0 +1,46 @@
+﻿namespace EasyMicroservices.Mapper.CompileTimeMapper.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TFrom"></typeparam>
+    /// <typeparam name="TTo"></typeparam>
+    public interface IMapper<TFrom, TTo> : IMapper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fromObject"></param>
+        /// <param name="uniqueRecordId"></param>
+        /// <param name="language"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public TTo Map(TFrom fromObject, string uniqueRecordId, string language, object[] parameters);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fromObject"></param>
+        /// <param name="uniqueRecordId"></param>
+        /// <param name="language"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public TFrom Map(TTo fromObject, string uniqueRecordId, string language, object[] parameters);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMapper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fromObject"></param>
+        /// <param name="uniqueRecordId"></param>
+        /// <param name="language"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public object MapObject(object fromObject, string uniqueRecordId, string language, object[] parameters);
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Mapper.CompileTimeMapper/Providers/CompileTimeMapperProvider.cs
@@ -0,0 +1,65 @@
+﻿using EasyMicroservices.Mapper.CompileTimeMapper.Interfaces;
+using EasyMicroservices.Mapper.Providers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace EasyMicroservices.Mapper.CompileTimeMapper.Providers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CompileTimeMapperProvider : BaseMapperProvider
+    {
+        Dictionary<Type, Dictionary<Type, IMapper>> Mappers { get; set; } = new Dictionary<Type, Dictionary<Type, IMapper>>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fromType"></param>
+        /// <param name="toType"></param>
+        /// <param name="mapper"></param>
+        public void AddMapper(Type fromType, Type toType, IMapper mapper)
+        {
+            AddMapperForType(fromType, toType, mapper);
+            AddMapperForType(toType, fromType, mapper);
+        }
+
+        void AddMapperForType(Type fromType, Type toType, IMapper mapper)
+        {
+            if (Mappers.TryGetValue(fromType, out Dictionary<Type, IMapper> mappers))
+            {
+                if (mappers.TryGetValue(toType, out _))
+                    throw new Exception($"Dupplicate mapper added for {fromType} and {toType}");
+                else
+                    mappers[toType] = mapper;
+            }
+            else
+                Mappers[fromType][toType] = mapper;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TTo"></typeparam>
+        /// <param name="fromObject"></param>
+        /// <param name="uniqueRecordId"></param>
+        /// <param name="language"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override TTo Map<TTo>(object fromObject, string uniqueRecordId = null, string language = null, params object[] parameters)
+        {
+            if (fromObject == null)
+                return default;
+            if (Mappers.TryGetValue(fromObject.GetType(), out Dictionary<Type, IMapper> mappers))
+            {
+                if (mappers.TryGetValue(typeof(TTo),out IMapper mapper))
+                {
+                    return (TTo)mapper.MapObject(fromObject, uniqueRecordId, language, parameters);
+                }
+            }
+            throw new Exception($"mapper not found for {fromObject.GetType()} and {typeof(TTo)}");
+        }
+    }
+}
